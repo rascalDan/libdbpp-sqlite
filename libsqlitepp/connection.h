1533d2a13778b7bd6a329a6294d1aae30de61288
Dan Goodliffe <dan@randomdan.homeip.net>
2015-10-20 00:30:53 +0100

    Add override attributes

--- libsqlitepp/connection.h
+++ libsqlitepp/connection.h
@@ -11,21 +11,21 @@ namespace SQLite {
 			Connection(const std::string & info);
 			~Connection();
 
-			void finish() const;
-			int beginTx() const;
-			int commitTx() const;
-			int rollbackTx() const;
-			bool inTx() const;
-			void ping() const;
-			DB::BulkDeleteStyle bulkDeleteStyle() const;
-			DB::BulkUpdateStyle bulkUpdateStyle() const;
+			void finish() const override;
+			int beginTx() const override;
+			int commitTx() const override;
+			int rollbackTx() const override;
+			bool inTx() const override;
+			void ping() const override;
+			DB::BulkDeleteStyle bulkDeleteStyle() const override;
+			DB::BulkUpdateStyle bulkUpdateStyle() const override;
 
-			DB::SelectCommand * newSelectCommand(const std::string & sql) const;
-			DB::ModifyCommand * newModifyCommand(const std::string & sql) const;
+			DB::SelectCommand * newSelectCommand(const std::string & sql) const override;
+			DB::ModifyCommand * newModifyCommand(const std::string & sql) const override;
 
-			void	beginBulkUpload(const char *, const char *) const;
-			void	endBulkUpload(const char *) const;
-			size_t bulkUploadData(const char *, size_t) const;
+			void	beginBulkUpload(const char *, const char *) const override;
+			void	endBulkUpload(const char *) const override;
+			size_t bulkUploadData(const char *, size_t) const override;
 
 			sqlite3 * db;
 
